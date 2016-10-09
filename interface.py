b5d1280bc5da0dbdaff8b110295d7a76e4c06396
Brian Kustra <bkustrakb@gmail.com>
2016-10-08 19:11:20 -0500

    Added a gitignore file, and made the interface more fun

--- interface.py
+++ interface.py
@@ -1,8 +1,16 @@
+# Type this in your console: python interface.py asdf
+# > asdf
+# Now type: python interface.py run bagua
+# > What will it say??
+
 from sys import argv
 
 def main():
     try:
-        print(argv[1])
+        if(argv[1] == "run" and argv[2] == "bagua"):
+          print("Excellent! Now let's get to work!")
+        else:
+          print(argv[1])
     except IndexError:
         print("Program Missing Arg. What should I-Ching do?")
     except Exception as e:
